fc075f3f83050704266e998a6f33b9cde6f32774
Antonio Román <kyradiscord@gmail.com>
2020-10-26 19:35:43 +0100

    chore: first commit

---
+++ scripts/build.ps1
@@ -0,0 +1,70 @@
+param (
+	[Alias("MsBuild")]
+	[Parameter(Position = 0)]
+	[ValidateScript( { Test-Path -LiteralPath $_ -PathType Leaf })]
+	[string]
+	$MsBuildPath = "",
+
+	[Alias("CMake")]
+	[string] $CMakePath,
+
+	# Configurations
+	[switch] $NDebug,
+	[switch] $NRelease
+)
+
+$ErrorActionPreference = "Stop"
+
+$local:RootFolder = Split-Path $PSScriptRoot -Parent
+$local:ModulesFolder = "$RootFolder\scripts\modules"
+$local:BinaryDirectory = "$RootFolder\bin"
+$local:DependenciesRoot = "$RootFolder\deps"
+
+$local:FreeAlutFolder = Join-Path -Path $DependenciesRoot -ChildPath "freealut"
+$local:OpenAlSoftFolder = Join-Path -Path $DependenciesRoot -ChildPath "openal-soft"
+
+Import-Module -Name (Join-Path -Path $ModulesFolder -ChildPath "MsBuild")
+Import-Module -Name (Join-Path -Path $ModulesFolder -ChildPath "CMake")
+Import-Module -Name (Join-Path -Path $ModulesFolder -ChildPath "Shared")
+
+# Find and assert MSBuild and CMake
+$local:MsBuild = Find-MsBuild -MsBuildPath $MsBuildPath
+$local:CMake = Find-CMake -CMakePath $CMakePath
+
+Try {
+	# Build OpenAL
+	Step-CMake $CMake $OpenAlSoftFolder @()
+	Step-VisualStudio -MsBuild $MsBuild -Path "$OpenAlSoftFolder\build\OpenAL.sln" -Configuration "Release"
+
+	Write-Host "Done! Library file written at "               -ForegroundColor Green -NoNewLine
+	Write-Host "$OpenAlSoftFolder\build\Release\OpenAL32.lib" -ForegroundColor Blue -NoNewLine
+	Write-Host "!"                                            -ForegroundColor Green
+
+	# Build FreeALUT
+	Step-CMake $CMake $FreeAlutFolder @(
+		"-DOPENAL_INCLUDE_DIR:PATH=$OpenAlSoftFolder\include\AL",
+		"-DOPENAL_LIBRARY:PATH=$OpenAlSoftFolder\build\Release\OpenAL32.lib"
+	)
+	Step-VisualStudio -MsBuild $MsBuild -Path "$FreeAlutFolder\build\Alut.sln" -Configuration "Release"
+	Copy-Item -Path "$FreeAlutFolder\build\src\Release\alut.dll" -Destination $BinaryDirectory
+
+	Write-Host "Done! Library file written at "             -ForegroundColor Green -NoNewLine
+	Write-Host "$FreeAlutFolder\build\src\Release\alut.lib" -ForegroundColor Blue -NoNewLine
+	Write-Host "! I also copied the .dll for you :)"        -ForegroundColor Green
+
+	Exit 0
+}
+Catch {
+	# Write the exception
+	Write-Host -Object $_
+	Write-Host -Object $_.Exception
+	Write-Host -Object $_.ScriptStackTrace
+
+	Exit 1
+}
+Finally {
+	# Unregister modules
+	Remove-Module MsBuild
+	Remove-Module CMake
+	Remove-Module Shared
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <cstdlib>
+#include <al.h>
+
+int main() {
+	return EXIT_SUCCESS;
+}
